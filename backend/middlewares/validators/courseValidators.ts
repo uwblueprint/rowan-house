3512eb0e03c7bc54a104f06948633ee233cde64d
e-wai <40974372+e-wai@users.noreply.github.com>
2022-01-31 07:34:56 -0800

    Implement course API (#21)
    Add course resolver and type
    
    Add course files to fill out
    
    Implement courseResolvers, course model, and course services
    
    Finish implementation of course service, implement course validators
    
    Fixups and rebase fixes
    
    Co-authored-by: binalpreetkalra <84669561+binalpreetkalra@users.noreply.github.com>
    
    Co-authored-by: binalpreetkalra <84669561+binalpreetkalra@users.noreply.github.com>

---
+++ backend/middlewares/validators/courseValidators.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import {
+  getApiValidationError,
+  validateArray,
+  validatePrimitive,
+} from "./util";
+import { getErrorMessage } from "../../utilities/errorUtils";
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable-next-line import/prefer-default-export */
+export const courseRequestDtoValidator = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let body;
+  try {
+    body = JSON.parse(req.body.body);
+  } catch (e: unknown) {
+    return res.status(400).send(getErrorMessage(e));
+  }
+  if (!validatePrimitive(body.title, "string")) {
+    return res.status(400).send(getApiValidationError("title", "string"));
+  }
+  if (!validatePrimitive(body.description, "string")) {
+    return res.status(400).send(getApiValidationError("description", "string"));
+  }
+  if (!validatePrimitive(body.image, "string")) {
+    return res.status(400).send(getApiValidationError("image", "string"));
+  }
+  if (!validatePrimitive(body.previewImage, "string")) {
+    return res
+      .status(400)
+      .send(getApiValidationError("previewImage", "string"));
+  }
+  if (!validateArray(body.lessons, "string")) {
+    return res
+      .status(400)
+      .send(getApiValidationError("lessons", "string", true));
+  }
+  return next();
+};
