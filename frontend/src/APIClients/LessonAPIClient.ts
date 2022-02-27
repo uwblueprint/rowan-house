579cf7467c7cbc810231129384758be2aa2902f6
Yewon Kwak <goodywk07@gmail.com>
2022-02-27 14:50:29 -0500

    Implement Lesson API Client (#59)
    * lesson api client
    
    * add token header
    
    * change content record type
    
    * update name of methods for consistency
    
    * eslint disable no explicit any
    
    * rename file, fix request path, fix id type

---
+++ frontend/src/APIClients/LessonAPIClient.ts
@@ -0,0 +1,89 @@
+import baseAPIClient from "./BaseAPIClient";
+import AUTHENTICATED_USER_KEY from "../constants/AuthConstants";
+import { getLocalStorageObjProperty } from "../utils/LocalStorageUtils";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export type LessonRequest = {
+  course: string;
+  title: string;
+  description: string;
+  image: string;
+  content: [Record<string, any>];
+};
+
+export type LessonResponse = {
+  id: string;
+  course: string;
+  title: string;
+  description: string;
+  image: string;
+  content: [Record<string, any>];
+};
+
+// TO DO: error handling and getAllLessons(?)
+
+const getLesson = async (id: string): Promise<LessonResponse> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.get(`/lesson/${id}`, {
+      headers: { Authorization: bearerToken },
+    });
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createLesson = async (lesson: LessonRequest): Promise<LessonResponse> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.post("/lesson", lesson, {
+      headers: { Authorization: bearerToken },
+    });
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateLesson = async (
+  id: string,
+  lesson: LessonRequest,
+): Promise<LessonResponse> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.put(`/lesson/${id}`, lesson, {
+      headers: { Authorization: bearerToken },
+    });
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteLesson = async (id: string): Promise<string> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.delete(`/lesson/${id}`, {
+      headers: { Authorization: bearerToken },
+    });
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default { getLesson, createLesson, updateLesson, deleteLesson };
