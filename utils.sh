51ff2cd8bb4c9b4f689f2f2f003205efc4017352
e-wai <40974372+e-wai@users.noreply.github.com>
2022-03-07 18:56:58 -0800

    Add frontend lint util command

--- utils.sh
+++ utils.sh
@@ -1,4 +1,7 @@
-if [[ "$1" = "lint" ]]
+if [[ "$1" = "belint" ]]
 then
     docker exec -it RHS-backend /bin/bash -c "yarn lint --fix"
+elif [[ "$1" = "felint" ]]
+then
+    docker exec -it RHS-frontend /bin/bash -c "yarn lint --fix"
 fi
