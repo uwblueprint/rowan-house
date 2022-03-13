e4f3dce1b1f3997cc29c3181502d79d099d2c53e
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2022-03-13 15:30:35 -0400

    Landing Page: Navigation Sidebar Fixes #60

--- frontend/src/components/common/SideBar.tsx
+++ frontend/src/components/common/SideBar.tsx
@@ -1,30 +1,74 @@
 import React from "react";
-import { Box, Image } from "@chakra-ui/react";
-import RHSLogo from "../../assets/RHSlogo.png";
+import {
+  Box,
+  Flex,
+  Square,
+  Text,
+  Center,
+  Spacer,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  VStack,
+  Avatar, 
+  AvatarBadge, 
+  AvatarGroup, 
+  Divider,
+  Heading
+} from "@chakra-ui/react";
 
-const SideBar = ({
-  children,
-}: {
-  children?: React.ReactNode;
-}): React.ReactElement => {
+
+const Sidebar = (): React.ReactElement => {
   return (
-    <Box
-      className="sidebar"
-      width="280px"
-      minWidth="280px"
-      backgroundColor="brand.royal"
-    >
-      <Image
-        src={RHSLogo}
-        alt="Rowan House logo"
-        width="150px"
-        mx={16}
-        my={8}
-      />
-      {children}
-      <p>Test element</p>
+    <Box w="20%">
+      <Flex
+        position="fixed"
+        flex = "1"
+        top="0"
+        left="0"
+        p="4"
+        w="20%"
+        h="100%"
+        minW="min-content"
+        boxShadow="xl"
+        flexFlow="column"
+        flexDir="column"
+        alignItems="center"
+        bg = "brand.royal"
+        color ="white"
+        orientation = "vertical"
+        justify="space=between"
+      >
+
+      <Flex w="100">
+        <Text>for options</Text>
+      </Flex>
+
+      
+
+      <Flex
+      p="0"
+      w="100%"
+      alignItems="inherit"
+      orientation = "horizontal"
+      flexDir="row"
+      >
+        <Divider />
+        <Flex w="100%">
+          <Avatar height="36px" width="36px"/>
+          <Flex flexDir="column" w="100%">
+            <Text variant> Admin</Text>
+            <Heading>Jane Doe</Heading>
+          </Flex>
+          
+        </Flex>
+
+      </Flex>
+      </Flex>
     </Box>
   );
 };
 
-export default SideBar;
+export default Sidebar;
