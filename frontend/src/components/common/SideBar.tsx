f1bd05dde520565caf0d67c8810f6227e998f732
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2022-03-13 15:30:35 -0400

    Landing Page: Navigation Sidebar Fixes #60

--- frontend/src/components/common/SideBar.tsx
+++ frontend/src/components/common/SideBar.tsx
@@ -4,24 +4,8 @@ import {
   Flex,
   Text,
   Spacer,
-<<<<<<< HEAD
   Image,
   Button
-=======
-  Tabs,
-  TabList,
-  Tab,
-  TabPanels,
-  TabPanel,
-  VStack,
-  Avatar, 
-  AvatarBadge, 
-  AvatarGroup, 
-  Divider,
-  Heading,
-  Icon,
-  Image
->>>>>>> 17a017a (work in progress 13/03)
 } from "@chakra-ui/react";
 
 import { DownloadIcon, SunIcon } from "@chakra-ui/icons";
@@ -66,22 +50,14 @@ const Sidebar = (): React.ReactElement => {
           width="150px"
           mx={16}
           my={8}
-<<<<<<< HEAD
           />
-=======
-        />
->>>>>>> 17a017a (work in progress 13/03)
       </Flex>
       
       <Flex 
       w="100"
       orientation = "vertical"
       flexDir = "column"
-<<<<<<< HEAD
       pt="72px">
-=======
-      pt="90px">
->>>>>>> 17a017a (work in progress 13/03)
 
         <Flex
         w="100"
@@ -89,16 +65,12 @@ const Sidebar = (): React.ReactElement => {
         orientation = "horizontal"
         flexDir = "row"
         >
-<<<<<<< HEAD
           <Image
           src={CoursesIcon}
           alt="Book Icon"
           width="24px"
           height="24px"
           />
-=======
-          <Icon as={SunIcon} w="24px" h="24px"/>
->>>>>>> 17a017a (work in progress 13/03)
           <Text variant="body" pl = "20px">Manage Courses</Text>
         </Flex>
 
@@ -108,16 +80,12 @@ const Sidebar = (): React.ReactElement => {
         orientation = "horizontal"
         flexDir = "row"
         >   
-<<<<<<< HEAD
           <Image
           src={UsersIcon}
           alt="Book Icon"
           width="24px"
           height="24px"
           />
-=======
-          <Icon as={SunIcon} w="24px" h="24px"/>
->>>>>>> 17a017a (work in progress 13/03)
           <Text variant="body" pl = "20px">Manage Users</Text>
         </Flex>
         
@@ -134,21 +102,13 @@ const Sidebar = (): React.ReactElement => {
       >
  
         <Flex w="100%" pl="10px" pt="8px" pb="16px" pr="32px">
-<<<<<<< HEAD
-=======
-          <Avatar height="36px" width="36px"/>
->>>>>>> 17a017a (work in progress 13/03)
           <Flex flexDir="column" w="100%" pl="24px">
             <Text variant = "caption" opacity="0.7"> Admin</Text>
             <Text variant= "body-bold">Jane Doe</Text>
           </Flex>
-<<<<<<< HEAD
           <Button variant="solid">
             <Text variant="button-sb" color="#2D3748">Sign Out</Text>
           </Button>
-=======
-          <Icon as={DownloadIcon} w="24px" h="16px"/>
->>>>>>> 17a017a (work in progress 13/03)
         </Flex>
 
       </Flex>
