7b98bbba0ec58efa4bc0f0e60873498243d163e1
e-wai <40974372+e-wai@users.noreply.github.com>
2022-03-13 03:47:53 -0700

    Setup skeleton of course overview page

--- frontend/src/components/common/SideBar.tsx
+++ frontend/src/components/common/SideBar.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+import RHSLogo from "../../assets/RHSlogo.png";
 
 const SideBar = ({
   children,
 }: {
   children?: React.ReactNode;
 }): React.ReactElement => {
-  return <div>{children}</div>;
+  return (
+    <Box
+      className="sidebar"
+      width="280px"
+      minWidth="280px"
+      backgroundColor="brand.royal"
+    >
+      <Image
+        src={RHSLogo}
+        alt="Rowan House logo"
+        width="150px"
+        mx={16}
+        my={8}
+      />
+      {children}
+      <p>Test element</p>
+    </Box>
+  );
 };
 
 export default SideBar;
