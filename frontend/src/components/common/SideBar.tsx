43a5247975245b159d4acb5ea67f3554e8eb0438
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2022-03-14 22:13:44 -0400

    Sidebar with images

--- frontend/src/components/common/SideBar.tsx
+++ frontend/src/components/common/SideBar.tsx
@@ -100,9 +100,19 @@ const Sidebar = (): React.ReactElement => {
             <Text variant = "caption" opacity="0.7"> Admin</Text>
             <Text variant= "body-bold">Jane Doe</Text>
           </Flex>
+<<<<<<< HEAD
           <Button variant="solid">
             <Text variant="button-sb" color="#2D3748">Sign Out</Text>
           </Button>
+=======
+          {/* <Icon as={DownloadIcon} w="24px" h="16px"/> */}
+          <IconButton
+          variant='ghost'
+          colorScheme='white'
+          aria-label='Log out'
+          icon={<DownloadIcon />}
+          />
+>>>>>>> 9290b0b (Sidebar with images)
         </Flex>
 
       </Flex>
