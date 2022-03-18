02429ab5dc1ec99c191fb486778b03b3cfc5020f
Yewon Kwak <goodywk07@gmail.com>
2022-03-18 11:30:55 -0400

    course api clients

---
+++ frontend/src/APIClients/mutations/CourseMutations.ts
@@ -0,0 +1,35 @@
+import { gql } from "@apollo/client"
+
+export const CREATE_COURSE = gql`
+  mutation CreateCourse($course: CreateCourseRequestDTO!) {
+    createCourse(course: $course) {
+      title
+      description
+      image
+      previewImage
+      lessons
+      private
+      published
+    }
+  }
+`;
+
+export const UPDATE_COURSE = gql`
+  mutation UpdateCourse($id: ID!, $course: UpdateCourseRequestDTO!) {
+    updateCourse(id: $id, course: $course) {
+      title
+      description
+      image
+      previewImage
+      lessons
+      private
+      published
+    }
+  }
+`;
+
+export const DELETE_COURSE = gql`
+  mutation DeleteCourse($id: ID!) {
+    deleteCourse(id: $id)
+  }
+`;
---
+++ frontend/src/APIClients/queries/CourseQueries.ts
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client"
+
+export const COURSES = gql`
+  query Courses {
+    courses {
+      title
+      description
+      image
+      previewImage
+      lessons
+      private
+      published
+    }
+  }
+`;
+
+export const GET_COURSE = gql`
+  query GetCourse($id: String) {
+    course(id: $id) {
+        title
+        description
+        image
+        previewImage
+        lessons
+        private
+        published
+    }
+  }
+`;
