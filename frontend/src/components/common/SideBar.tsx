91227b2d2c41377e83ff661912ba76eff64867e2
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2022-03-27 15:56:15 -0400

    fixed problems

--- frontend/src/components/common/SideBar.tsx
+++ frontend/src/components/common/SideBar.tsx
@@ -1,118 +1,70 @@
 import React from "react";
-import {
-  Box,
-  Flex,
-  Text,
-  Spacer,
-  Image,
-  Button
-} from "@chakra-ui/react";
-
+import { Box, Flex, Text, Spacer, Image, Button } from "@chakra-ui/react";
 
 import CoursesIcon from "../../assets/Courses.svg";
 import UsersIcon from "../../assets/Users.svg";
 import RHSLogo from "../../assets/RHSLogo-white.png";
 
+interface TabProps {
+    icon:string
+    text:string
+}
 
-
-
-
-import CoursesIcon from "../../assets/Courses.svg";
-import UsersIcon from "../../assets/Users.svg";
-import RHSLogo from "../../assets/RHSLogo-white.png";
-
-
+function Tab({icon, text}:TabProps) {
+   return(
+    <Flex padding="1rem 2rem">
+      <Image
+        src={icon}
+        alt={text}
+        width="1.5rem"
+        height="1.5rem"
+        />
+      <Text variant="body" pl="1.5rem">
+        {text}
+      </Text>
+    </Flex>
+   )
+ }
 
 const Sidebar = (): React.ReactElement => {
   return (
     <Box w="20%">
       <Flex
         position="fixed"
-        flex = "1"
-        top="0"
-        left="0"
-        p="4px"
-        w="280px"
+        flex="1"
+        p="0.25px"
+        w="20%"
         h="100%"
         minW="min-content"
-        boxShadow="xl"
         flexFlow="column"
-        flexDir="column"
-        alignItems="center"
-        bg = "brand.royal"
-        color ="white"
-        orientation = "vertical"
+        align="center"
+        bg="brand.royal"
+        color="white"
         justify="space=between"
       >
-      
-      <Flex>
-        <Image
-          src={RHSLogo}
-          alt="Rowan House logo"
-          width="150px"
-          mx={16}
-          my={8}
-          />
-      </Flex>
-      
-      <Flex 
-      w="100"
-      orientation = "vertical"
-      flexDir = "column"
-      pt="72px">
-
-        <Flex
-        w="100"
-        h="62px"
-        orientation = "horizontal"
-        flexDir = "row"
-        >
+        <Flex>
           <Image
-          src={CoursesIcon}
-          alt="Book Icon"
-          width="24px"
-          height="24px"
+            src={RHSLogo}
+            alt="Rowan House logo"
+            width="10rem"
+            pt="2rem"
           />
-          <Text variant="body" pl = "20px">Manage Courses</Text>
         </Flex>
-
+        <Flex w="100%" flexDir="column" pt="72px">
+          <Tab icon={CoursesIcon} text="Manage Courses"/>
+          <Tab icon={UsersIcon} text="Manage Users"/>
+        </Flex>
+        <Spacer />
         <Flex
-        w="100"
-        h="62px"
-        orientation = "horizontal"
-        flexDir = "row"
-        >   
-          <Image
-          src={UsersIcon}
-          alt="Book Icon"
-          width="24px"
-          height="24px"
-          />
-          <Text variant="body" pl = "20px">Manage Users</Text>
-        </Flex>
-        
-      </Flex>
-
-      
-      <Spacer/>
-      <Flex
-      p="0"
-      w="100%"
-      alignItems="inherit"
-      orientation = "vertical"
-      flexDir="column"
+        w="100%"
+        justify="space-between"
+        p="1.5rem"
       >
- 
-        <Flex w="100%" pl="10px" pt="8px" pb="16px" pr="32px">
-          <Flex flexDir="column" w="100%" pl="24px">
-            <Text variant = "caption" opacity="0.7"> Admin</Text>
-            <Text variant= "body-bold">Jane Doe</Text>
+           <Flex flexDir="column">
+            <Text variant="caption" opacity="0.7"> Admin</Text>
+            <Text variant="body-bold">Jane Doe</Text>
           </Flex>
-          <Button variant="solid">
-            <Text variant="button-sb" color="#2D3748">Sign Out</Text>
-          </Button>
-        </Flex>
-
+          <Button variant="secondary-filled">Sign Out</Button>
       </Flex>
       </Flex>
     </Box>
