afa78cbf979c0b9186603514b59731609306aec9
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2022-03-31 21:22:18 -0400

    Removed import from AdminDashboard

--- frontend/src/components/pages/AdminDashboard.tsx
+++ frontend/src/components/pages/AdminDashboard.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Flex } from "@chakra-ui/react";
 import SideBar from "../admin/SideBar";
 import CoursesOverviewTab from "../admin/CoursesOverviewTab";
-import ManageTab from "../admin/ManageTab";
 
 const AdminDashboard = (): React.ReactElement => {
   return (
