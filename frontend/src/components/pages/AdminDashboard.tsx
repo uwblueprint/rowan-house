babb814ce76f25bad032745368f7abd00f5cf33a
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2022-03-31 21:41:01 -0400

    Admin Sidebar V1 (#101)
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * work in progress 13/03
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Sidebar with images
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Rebased
    
    * Adding svg
    
    * work in progress 13/03
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Sidebar with images
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Fixes pt 1
    
    * fixed problems
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Sidebar with images
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Rebased
    
    * Adding svg
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * work in progress 13/03
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Sidebar with images
    
    * Landing Page: Navigation Sidebar
    Fixes #60
    
    * Fixes pt 1
    
    * rebased
    
    * changed pixels to rem and edited stylization comments
    
    * linter issues fixed
    
    * Added Flexbos to AdminDashboard
    
    * removed redundencies
    
    * last commit
    
    * Removed import from AdminDashboard

--- frontend/src/components/pages/AdminDashboard.tsx
+++ frontend/src/components/pages/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
-import SideBar from "../common/SideBar";
+import SideBar from "../admin/SideBar";
 import CoursesOverviewTab from "../admin/CoursesOverviewTab";
 
 const AdminDashboard = (): React.ReactElement => {
