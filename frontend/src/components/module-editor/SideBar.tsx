5c6fdabf662f651ba223d5853f608a8c67c987bf
Yewon Kwak <goodywk07@gmail.com>
2022-04-23 13:15:40 -0400

    Module Editor Sidebar Skeleton (#113)
    * initial skeleton
    
    * add scroll, save icon, image
    
    * fix tab width, save icon
    
    * fix icon

--- frontend/src/components/module-editor/SideBar.tsx
+++ frontend/src/components/module-editor/SideBar.tsx
@@ -1,39 +1,103 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
 import {
   Box,
+  Button,
   Flex,
+  Link,
+  HStack,
   Tabs,
   TabList,
   Tab,
   TabPanels,
   TabPanel,
+  Text,
 } from "@chakra-ui/react";
+import { ChevronLeftIcon, EditIcon } from "@chakra-ui/icons";
 
 import ModuleOverview from "./SideBarModuleOverview";
 import ContentKiosk from "./SideBarContentKiosk";
+import { ReactComponent as SaveIcon } from "../../assets/Save.svg";
+import { ADMIN_DASHBOARD_PAGE } from "../../constants/Routes";
+import EditorContext from "../../contexts/ModuleEditorContext";
+import { ModuleEditorParams } from "../../types/ModuleEditorTypes";
 
 const Sidebar = (): React.ReactElement => {
+  const { moduleIndex }: ModuleEditorParams = useParams();
+
+  const context = useContext(EditorContext);
+
+  if (!context) return <></>;
+  const { state } = context;
   return (
-    <Box w="20%">
+    <Box w="20%" minW="300px">
       <Flex
         position="fixed"
-        top="0"
-        left="0"
-        p="4"
         w="inherit"
-        h="100%"
-        minW="min-content"
+        minW="300px"
+        h="100vh"
         boxShadow="xl"
         flexFlow="column"
-        alignItems="center"
       >
-        <Tabs variant="soft-rounded">
-          <TabList>
-            <Tab>Overview</Tab>
-            <Tab>Components</Tab>
-          </TabList>
+        <Box opacity="0.9" backgroundColor="black">
+          <Flex
+            h="240px"
+            backgroundPosition="center"
+            backgroundImage={
+              state.course.modules[Number(moduleIndex)].previewImage
+            }
+            backgroundSize="cover"
+            bgRepeat="no-repeat"
+            opacity="1"
+            direction="column"
+            justifyContent="space-between"
+            p="35px"
+          >
+            <HStack justify="space-between">
+              <Link href={ADMIN_DASHBOARD_PAGE}>
+                <ChevronLeftIcon color="white" h={6} w={6} />
+              </Link>
+              <EditIcon color="white" h={5} w={5} />
+            </HStack>
+            <Text variant="display-sm-sb" color="white">
+              {state.course.modules[Number(moduleIndex)].title}
+            </Text>
+          </Flex>
+        </Box>
+        <Tabs variant="unstyled">
+          <Box
+            bg="background.light"
+            borderRadius="md"
+            p="6px"
+            margin="auto"
+            maxW="min-content"
+            my="43px"
+          >
+            <TabList>
+              <Tab
+                style={{ paddingInline: "35px", height: "32px" }}
+                _selected={{
+                  color: "white",
+                  bg: "brand.royal",
+                  borderRadius: "md",
+                }}
+              >
+                Overview
+              </Tab>
+              <Tab
+                style={{ paddingInline: "23px", height: "32px" }}
+                _selected={{
+                  color: "white",
+                  bg: "brand.royal",
+                  borderRadius: "md",
+                }}
+              >
+                Components
+              </Tab>
+            </TabList>
+          </Box>
           <TabPanels>
-            <TabPanel>
+            <TabPanel p="0">
               <ModuleOverview />
             </TabPanel>
             <TabPanel>
@@ -41,6 +105,22 @@ const Sidebar = (): React.ReactElement => {
             </TabPanel>
           </TabPanels>
         </Tabs>
+        {state.hasChanged && (
+          <Button
+            position="fixed"
+            bottom="0"
+            bg="#5FCA89"
+            color="white"
+            leftIcon={<SaveIcon />}
+            borderRadius="0"
+            pl="35px"
+            minW="inherit"
+            h="55px"
+            justifyContent="left"
+          >
+            Save Changes
+          </Button>
+        )}
       </Flex>
     </Box>
   );
--- frontend/src/components/module-editor/SideBarModuleOverview.tsx
+++ frontend/src/components/module-editor/SideBarModuleOverview.tsx
@@ -1,4 +1,5 @@
 import { Button, VStack } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 
@@ -13,8 +14,9 @@ const SideBarModuleOverview = (): React.ReactElement => {
   if (!context) return <></>;
   const { state, dispatch } = context;
 
-  const { lessons, course } = state;
+  const { lessons, course, focusedLesson } = state;
   const module = course.modules[moduleID];
+  if (!focusedLesson) return <></>;
 
   const orderedLessons = module.lessons.map((id) => lessons[id]);
 
@@ -33,26 +35,56 @@ const SideBarModuleOverview = (): React.ReactElement => {
     });
 
   return (
-    <VStack>
-      {module.title}
-      {module.description}
-      <br />
-      {orderedLessons.map(
-        (lesson, index) =>
-          lesson && (
-            <Button key={index} onClick={() => setFocus(index)}>
+    <VStack h="50%" position="absolute" width="100%" pb="55px">
+      <VStack className="tabScroll" overflowY="scroll" width="inherit">
+        {orderedLessons.map((lesson, index) =>
+          state.lessons[focusedLesson] === lesson ? (
+            lesson && (
+              <Button
+                key={index}
+                onClick={() => setFocus(index)}
+                variant="unstyled"
+                borderLeftColor="brand.royal"
+                borderLeftWidth="5px"
+                borderRadius="0"
+                bg="background.light"
+                textAlign="left"
+                pl="30px"
+                minH="55px"
+                w="100%"
+              >
+                {lesson.title}
+              </Button>
+            )
+          ) : (
+            <Button
+              key={index}
+              onClick={() => setFocus(index)}
+              variant="unstyled"
+              textAlign="left"
+              pl="35px"
+              minH="55px"
+              w="100%"
+            >
               {lesson.title}
             </Button>
           ),
-      )}
-      <Button
-        onClick={() =>
-          createLesson(`Dummy Lesson ${orderedLessons.length + 1}`)
-        }
-      >
-        Create new Lesson
-      </Button>
-      <p>Should we save? {state.hasChanged ? "yes" : "no"}</p>
+        )}
+        <Button
+          onClick={() =>
+            createLesson(`Dummy Lesson ${orderedLessons.length + 1}`)
+          }
+          color="brand.royal"
+          variant="unstyled"
+          leftIcon={<AddIcon />}
+          textAlign="left"
+          pl="35px"
+          minH="55px"
+          w="100%"
+        >
+          New Lesson
+        </Button>
+      </VStack>
     </VStack>
   );
 };
