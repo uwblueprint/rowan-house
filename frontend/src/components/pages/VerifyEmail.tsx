a19da0cf9ba7af70ca532a2c0e63098cd28662cb
Victor Zheng <vzheng@gmail.com>
2022-05-18 22:27:47 -0400

    verify page and route done

---
+++ frontend/src/components/pages/VerifyEmail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = (): React.ReactElement => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Please Verify Your Email! </h1>
+    </div>
+  );
+};
+
+export default NotFound;
