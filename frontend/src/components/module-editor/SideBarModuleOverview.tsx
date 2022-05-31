4d338bcafdf911b1d4f23125026488660562082d
Molly Yu <55213953+molly-yu@users.noreply.github.com>
2022-05-30 22:47:42 -0400

    Change lesson names (#150)
    * Add modal for naming lessons
    
    * Add error state for empty title
    
    * Update frontend/src/components/module-editor/SideBarModuleOverview.tsx
    
    Co-authored-by: Matthew Wang <53355975+mslwang@users.noreply.github.com>
    
    * fix naming
    
    * Reset state on cancel
    
    * Fix linting
    
    * Fix styling
    
    Co-authored-by: Matthew Wang <53355975+mslwang@users.noreply.github.com>

--- frontend/src/components/module-editor/SideBarModuleOverview.tsx
+++ frontend/src/components/module-editor/SideBarModuleOverview.tsx
@@ -1,13 +1,19 @@
-import { Button, VStack } from "@chakra-ui/react";
+import { Button, useDisclosure, VStack } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import EditorContext from "../../contexts/ModuleEditorContext";
 import { ModuleEditorParams } from "../../types/ModuleEditorTypes";
+import { Modal } from "../common/Modal";
+import { TextInput } from "../common/TextInput";
 
 const SideBarModuleOverview = (): React.ReactElement => {
   const context = useContext(EditorContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [title, setTitle] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const { courseID, moduleIndex }: ModuleEditorParams = useParams();
   const moduleID = parseInt(moduleIndex, 10);
 
@@ -22,16 +28,30 @@ const SideBarModuleOverview = (): React.ReactElement => {
   const setFocus = (index: number) =>
     dispatch({ type: "set-focus", value: module.lessons[index] });
 
-  const createLesson = (title: string) =>
-    dispatch({
-      type: "create-lesson",
-      value: {
-        course: courseID,
-        module: course.modules[moduleID].id,
-        title,
-        content: [],
-      },
-    });
+  const resetState = () => {
+    setTitle("");
+    setErrorMessage("");
+    setIsInvalid(false);
+  };
+
+  const createLesson = (lessonTitle: string) => {
+    if (title) {
+      dispatch({
+        type: "create-lesson",
+        value: {
+          course: courseID,
+          module: course.modules[moduleID].id,
+          title: lessonTitle,
+          content: [],
+        },
+      });
+      resetState();
+      onClose();
+    } else {
+      setErrorMessage("Error: title cannot be empty.");
+      setIsInvalid(true);
+    }
+  };
 
   return (
     <VStack>
@@ -70,7 +90,7 @@ const SideBarModuleOverview = (): React.ReactElement => {
           ),
         )}
       <Button
-        onClick={() => createLesson(`Lesson ${orderedLessons.length + 1}`)}
+        onClick={onOpen}
         color="brand.royal"
         variant="unstyled"
         leftIcon={<AddIcon />}
@@ -81,6 +101,26 @@ const SideBarModuleOverview = (): React.ReactElement => {
       >
         New Lesson
       </Button>
+      <Modal
+        header="Edit lesson title"
+        isOpen={isOpen}
+        onConfirm={() => createLesson(title)}
+        onCancel={() => {
+          resetState();
+          onClose();
+        }}
+      >
+        <TextInput
+          placeholder="New Lesson"
+          onChange={(currTitle) => {
+            setTitle(currTitle);
+            setIsInvalid(false);
+          }}
+          errorMessage={errorMessage}
+          isInvalid={isInvalid}
+          isRequired
+        />
+      </Modal>
     </VStack>
   );
 };
