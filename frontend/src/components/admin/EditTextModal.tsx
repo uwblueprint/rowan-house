13dd297e99a9bfeae5d5d29dfe5a60007033349c
molly-yu <55213953+molly-yu@users.noreply.github.com>
2022-06-06 22:21:12 -0400

    Add edit text modal with automatic block updating

---
+++ frontend/src/components/admin/EditTextModal.tsx
@@ -0,0 +1,59 @@
+import { Flex, VStack, Image } from "@chakra-ui/react";
+import React, { useContext, useState } from "react";
+
+import { TextInput } from "../common/TextInput";
+import { Modal } from "../common/Modal";
+import { ContentBlock, ContentTypeEnum, EditorContextAction } from "../../types/ModuleEditorTypes";
+import EditorContext from "../../contexts/ModuleEditorContext";
+import { TextArea } from "../common/TextArea";
+
+export interface EditTextModalProps {
+  onClose: () => void;
+  isOpen: boolean;
+  block: ContentBlock;
+  index: number;
+}
+
+const EditTextModal = ({
+  block,
+  index,
+  isOpen,
+  onClose,
+}: EditTextModalProps): React.ReactElement => {
+  const context = useContext(EditorContext);
+  const [text, setText] = useState(block.content.text ?? "");
+
+  if (!context) return <></>;
+  const { state, dispatch } = context;
+
+  const onSave = () => {
+    const newBlock = block;
+    newBlock.content.text = text;
+    dispatch({
+      type: "update-block",
+      value: {
+        index,
+        block,
+      },
+    });
+  }
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Modal
+      size="xl"
+      header="Edit Text Component"
+      onConfirm={onSave}
+      onCancel={onClose}
+      isOpen={isOpen}
+    >
+      <Flex>
+        <VStack flex="1">
+          <TextArea defaultValue={text} onChange={setText} isRequired />
+        </VStack>
+      </Flex>
+    </Modal>
+  );
+};
+
+export default EditTextModal;
