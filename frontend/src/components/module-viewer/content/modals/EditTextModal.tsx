48fb8c6936bd55bdd3ca05e580e66cbbd69909a3
Daniel Raymond <dan@raymond.ch>
2022-08-26 09:00:01 -0400

    Added slate library

--- frontend/src/components/module-viewer/content/modals/EditTextModal.tsx
+++ frontend/src/components/module-viewer/content/modals/EditTextModal.tsx
@@ -1,10 +1,30 @@
-import { Flex, VStack } from "@chakra-ui/react";
+import { Box, Flex, VStack } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { createEditor, BaseEditor, Descendant } from 'slate'
+import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
+
 import { TextBlockState } from "../../../../types/ContentBlockTypes";
 import { Modal } from "../../../common/Modal";
 
-import { TextArea } from "../../../common/TextArea";
 import { EditContentModalProps } from "../../../../types/ModuleEditorTypes";
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initialValue: Array<CustomElement> = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
 
 const EditTextModal = ({
   block,
@@ -12,6 +32,7 @@ const EditTextModal = ({
   onClose,
   onSave,
 }: EditContentModalProps<TextBlockState>): React.ReactElement => {
+  const [editor] = useState(() => withReact(createEditor()));
   const [text, setText] = useState(block.content.text ?? "");
   const [invalid, setInvalid] = useState(false);
 
@@ -33,13 +54,18 @@ const EditTextModal = ({
     >
       <Flex>
         <VStack flex="1">
-          <TextArea
+          <Box borderWidth="1px" w="100%" padding="1rem">
+            <Slate editor={editor} value={initialValue}>
+              <Editable />
+            </Slate>
+          </Box>
+          {/* <TextArea
             label="Text Content:"
             defaultValue={text}
             onChange={setText}
             isInvalid={invalid}
             errorMessage="This field is required."
-          />
+          /> */}
         </VStack>
       </Flex>
     </Modal>
