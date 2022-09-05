6bc9f97aadaad345724eb2e06660dc1722c29cf5
Matthew Wang <mslwang2001@gmail.com>
2022-09-04 22:11:35 -0400

    Add first iteration of next module card

--- frontend/src/components/module-viewer/NextModuleCard.tsx
+++ frontend/src/components/module-viewer/NextModuleCard.tsx
@@ -1,13 +1,6 @@
 import React, { useState } from "react";
-import {
-  Box,
-  Collapse,
-  Flex,
-  Heading,
-  Icon,
-  Image,
-  Spacer,
-} from "@chakra-ui/react";
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 import { DEFAULT_IMAGE } from "../../constants/DummyData";
 import RouterLink from "../common/RouterLink";
 import { COURSE_OVERVIEW_BASE_ROUTE } from "../../constants/Routes";
@@ -37,43 +30,59 @@ const NextModuleCard = ({
       onBlur={handleBlur}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      width="67%"
+      height="10%"
     >
       <Flex
-        boxShadow="0px 4px 12px rgba(0, 0, 0, 0.1)"
-        direction="column"
-        width="240px"
-        height="300px"
-        borderRadius="8px"
+        borderRadius="0.5em"
         overflow="hidden"
+        position="relative"
+        alignItems="center"
+        justifyContent="space-around"
+        border="solid"
+        borderWidth="0.15em"
+        borderColor={hovered ? "brand.royal" : "#F4F4F4"}
+        padding="1em"
       >
-        <Collapse startingHeight="120px" endingHeight="105px" in={expanded}>
-          <Image
-            src={DEFAULT_IMAGE}
-            alt="course-preview"
-            width="100%"
-            height="100%"
-            fit="cover"
-          />
-        </Collapse>
+        <Image
+          src={DEFAULT_IMAGE}
+          alt="course-preview"
+          width="5em"
+          height="5em"
+          fit="cover"
+          borderRadius="0.15em"
+        />
         <Flex direction="column" padding="24px" flex={1}>
-          <Box>
-            <Heading as="h3" size="md" marginBottom="6px">
-              insert course title
-            </Heading>
-          </Box>
-          <Box
+          <Flex
+            justifyItems="space-around"
+            alignItems="center"
+            marginBottom="0.5em"
+          >
+            <Flex
+              width="1em"
+              height="1em"
+              backgroundColor="#2F855A"
+              borderRadius="50%"
+            />
+            <Text marginLeft="0.5em" color="#2F855A">
+              In Progress
+            </Text>
+          </Flex>
+          <Heading
             textOverflow="ellipsis"
             overflow="hidden"
             display="-webkit-box"
+            color="#666666"
+            size="md"
             sx={{
               "-webkit-line-clamp": expanded ? "3" : "2",
               "-webkit-box-orient": "vertical",
             }}
           >
-            Description description description description description
-            description description
-          </Box>
+            Module {nextModuleIndex + 1}: Next Module Title
+          </Heading>
         </Flex>
+        <ChevronRightIcon w={6} h={6} color="brand.royal" />
       </Flex>
     </RouterLink>
   );
