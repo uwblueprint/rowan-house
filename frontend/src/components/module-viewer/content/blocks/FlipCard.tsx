e355c432c5c751202c6d4861ae1872375d706dd4
Daniel Raymond <dan@raymond.ch>
2022-09-10 14:28:18 +0200

    Working flipping

--- frontend/src/components/module-viewer/content/blocks/FlipCard.tsx
+++ frontend/src/components/module-viewer/content/blocks/FlipCard.tsx
@@ -1,22 +1,26 @@
-import { Center } from "@chakra-ui/react";
+import { Center, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 const FlipBlock = ({ front, back }: { front: string, back: string }): React.ReactElement => {
   const [flipped, setFlipped] = useState(false);
 
   return (
-    <Center
-      minW="10rem"
-      w="20%"
-      padding="2rem"
-      borderTopColor="brand.royal"
-      borderTopWidth="7px"
-      borderRadius="4px"
-      boxShadow="lg"
-      onClick={() => setFlipped(!flipped)}
-    >
-      {flipped ? back : front}
-    </Center>
+    <div className="card-container">
+      <Center
+        minW="10rem"
+        w="20%"
+        padding="2rem"
+        borderTopColor="brand.royal"
+        borderTopWidth="7px"
+        borderRadius="4px"
+        boxShadow="lg"
+        className={`card ${flipped ? 'card-flipped' : ''}`}
+        onClick={() => setFlipped(!flipped)}
+      >
+        <Text className="front">{ front }</Text>
+        <Text className="back">{ back }</Text>
+      </Center>
+    </div>
   );
 };
 
--- frontend/src/components/module-viewer/content/modals/EditFlipCardModal.tsx
+++ frontend/src/components/module-viewer/content/modals/EditFlipCardModal.tsx
@@ -50,7 +50,7 @@ const EditFlipCardModal = ({
 
   return (
     <Modal
-      header="Edit FlipCarding"
+      header="Edit Flip Cards"
       size="xl"
       onConfirm={onConfirm}
       onCancel={onClose}
